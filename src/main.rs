0b7ceb3af26b455afb7f3bd69e04a63dab449bc4
Kenz <kenz.bensmaine@gmail.com>
2022-11-10 15:35:10 +0100

    Add more comments

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,17 @@ const CURSOR_X: u16 = 0;
 const CURSOR_Y: u16 = 1;
 fn main() {
     //setting up stdout and going into raw mode
-    let (sender, receiver) = mpsc::channel();
     let mut stdout = stdout().into_raw_mode().unwrap();
     //printing welcoming message, clearing the screen and going to left top corner with the cursor
     write!(stdout, r#"{}{}Welcome to my new game it should be a rusty flappy bird with some useless features just to learn rust""#, termion::cursor::Goto(CURSOR_X, CURSOR_Y), termion::clear::All)
             .unwrap();
     stdout.flush().unwrap();
 
+    // Create Sender and Receiver object to comunicte thgrough threads
+    let (sender, receiver) = mpsc::channel();
+    // Create seperate thread for detecting input user and gives him the sender object
     thread::spawn(move || detect_user_input(sender));
+
     loop{
         match receiver.try_recv() {
             Ok('w') => println!("w is received!"),
@@ -31,15 +34,14 @@ fn main() {
         };
     }
 
+    println!("Game ended!");
     stdout.flush().unwrap();
 }
 
 fn detect_user_input(sender: Sender<char>){
     let stdin = stdin();
-    // let mut stdout = stdout().into_raw_mode().unwrap();
     //detecting keydown events
     for c in stdin.keys() {
-        //i reckon this speaks for itself
         match c.unwrap() {
             Key::Char('w') => sender.send('w').unwrap(),
             Key::Char('a') => sender.send('a').unwrap(),
